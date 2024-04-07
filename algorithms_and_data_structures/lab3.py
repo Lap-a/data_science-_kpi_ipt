ffb3b7e96ae63dd860452f7551d01e1d4d8e730b
Denys <denyslapt-ipt27@lll.kpi.ua>
2024-04-07 14:22:59 +0300

    add insert_after

--- algorithms_and_data_structures/lab3.py
+++ algorithms_and_data_structures/lab3.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, key):
+        self.key = key
         self.prev = None
         self.next = None
 
@@ -10,46 +10,61 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        #self.current = None
+        self.current = None
+
     def empty(self):
         return self.head is None
-    
-    def append(self, data):# at the end
-        new_node = Node(data)
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.key == key:
+                return current
+            current = current.next
+        return False
+
+    def append(self, key):# at the end
+        new_node = Node(key)
         if self.empty():
-            self.head = self.tail = new_node
+            self.head = self.tail = self.current = new_node
         else:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
 
-    def prepend(self, data):# at the start
-        new_node = Node(data)
+    def prepend(self, key):# at the start
+        new_node = Node(key)
         if self.empty():
-            self.head= self.tail = new_node
+            self.head = self.tail = self.current = new_node
         else:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
+        
+    def insert_after(self, key, new_key):
+        current = self.search(key)
+        if current == False:
+            print("No such element")
+        else:
+            if current.next:
+                new_node = Node(new_key)
+                new_node.prev = current
+                new_node.next = current.next
+                current.next.prev = new_node
+                current.next = new_node
+            else:
+                self.append(new_key)
 
     def print_list(self):
         current = self.head
         text = ""
         while current:
-            text += str(current.data) + " "
+            text += str(current.key) + " "
             current = current.next
         return text
 
-    def search(self, data):
-        current = self.head
-        while current:
-            if current.data == data:
-                return current
-            current = current.next
-        return False
-
-    def remove(self, data):
-        current = self.search(data)
+    def remove(self, key):
+        current = self.search(key)
         if current == False:
             print ("no such element")
         else:
@@ -59,14 +74,13 @@ class DoublyLinkedList:
                 self.head = current.next
             if current.next:
                 current.next.prev = current.prev
-            return
 
 
 
 llist = DoublyLinkedList()
-#llist.append("1")
-#llist.append("2")
-#llist.append("3")
+llist.append("1")
+llist.append("2")
+llist.append("3")
 
 wind = Tk()
 wind.title("lab3")
@@ -84,9 +98,8 @@ def rm():
 def show():
     lbl3.configure(text = "your list: " + llist.print_list())
 
-#lbl = Label(wind, text = "enter text"); lbl.grid()
-
-#entry = Entry(wind, width=57); entry.grid(column = 0, row = 1)
+def ins():
+    llist.insert_after(enter4.get(), enter3.get())
 
 lbl2 = Label(wind, text="enter key", width=30)
 lbl2.grid()
@@ -104,10 +117,19 @@ btn3 = Button(wind, text = "prepend", fg = "red", command=prep)
 btn3.grid(column=1, row=2)
 
 btn3 = Button(wind, text = "show", fg = "red", command=show)
-btn3.grid(column=0, row=7)
+btn3.grid(column=0, row=4)
+
+enter3 = Entry(wind, width=20)
+enter3.grid(column=1, row=5)
+
+btn4 = Button(wind, text = "insert after", fg = "red", command=ins)
+btn4.grid(column=1, row=6)
+
+enter4 = Entry(wind, width=20)
+enter4.grid(column=1, row=7)
 
 lbl3 = Label(wind, text = "your list: " + llist.print_list())
-lbl3.grid(column=0, row=8)
+lbl3.grid(column=0, row=5)
 
 
 wind.mainloop()
